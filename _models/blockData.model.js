1c528664a140ab10c25014c9d2384bf68748b7b4
Unknown <thomas_641991@hotmail.com>
2020-01-14 13:20:43 +0100

    Start blockchain

---
+++ _models/blockData.model.js
@@ -0,0 +1,10 @@
+class BlockDataModel {
+
+    constructor(patientName, employeeName, startTime, endTime, description) {
+        this.patientName = patientName;
+        this.employeeName = employeeName;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.description = description;
+    }
+}
